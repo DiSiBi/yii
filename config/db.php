6bbbbf5672459d9a020d191761fef04849c998c2
Nikolay Beliy <disibi@mail.ru>
2023-01-11 15:58:38 +0300

    Setting up project

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=' . env('POSTGRES_HOST') . ';dbname=' . env('POSTGRES_DB'),
+    'username' => env('POSTGRES_USER'),
+    'password' => env('POSTGRES_PASSWORD'),
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
